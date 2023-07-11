627f9642fbb6949f5520ac4382f966146ec0c072
CarolejaneCosmos <108039782+CarolejaneCosmos@users.noreply.github.com>
2023-07-11 09:37:19 +0800

    Readme.R

---
+++ Logo/Readme.R
@@ -0,0 +1,12 @@
+# ISSCAS and CSAES Logos
+This repository contains logo image files for the Institute of Soil Science, Chinese Academy of Sciences (ISSCAS) and the Changshu National Agro-ecosystem Observation and Research Station (CSAES).
+## Logos
+The logos are provided in PNG format:
+Logo1.png
+Logo2.png
+## Usage
+These logos may be used to represent ISSCAS and CSAES in documents, presentations, and other materials. Proper attribution is required. For example:
+"ISSCAS logo by Institute of Soil Science, Chinese Academy of Sciences"
+"CSAES logo by Changshu National Agro-ecosystem Observation and Research Station"
+## License
+The logos are copyright ISSCAS and CSAES respectively. Please contact for permission to use.
